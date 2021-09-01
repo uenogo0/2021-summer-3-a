8b3cd939c2ef32e7efd1e1e2ad2b37ed0a9b522c
kome252 <87895014+kome252@users.noreply.github.com>
2021-09-01 12:01:39 +0900

    途中経過８

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-import{Server}from"https://js.sabae.cc/Server.js";
-new Server(8001);
+import { Server } from "https://js.sabae.cc/Server.js";
+class MyServer extends Server {
+    api(path, req) {
+        console.log("path = ", path, "req = ", req)
+        switch (path) {
+            case "/api/follow": {
+                const resp = {
+                    count: 1
+                }
+                console.log("resp = ", resp)
+                return resp
+            }
+        }
+    }
+}
+new MyServer(8001);
